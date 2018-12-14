bf921933d0268fea8fa8825244703aeb1bc3b118
王文奇 <wangwenqi@orangelab.cn>
2018-12-14 19:04:02 +0800

    add a while sentence

---
+++ src/study/s_181210/Lesson.java
@@ -0,0 +1,14 @@
+package study.s_181210;
+
+/**
+ * 循环语句
+ */
+public class Lesson {
+    public static void main(String[] args) {
+        int number = 10;
+        while (number > 0) {
+            number--;
+            System.out.println("执行减法后，number = " + number);
+        }
+    }
+}
